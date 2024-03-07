64199a6784d1130c3203b86f2eee6252d1de0667
Jey Prabu <135853635+jeyprabu@users.noreply.github.com>
2024-03-07 14:39:52 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -6,11 +6,7 @@ def features(image, filename):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     dst = cv.cornerHarris(gray, 2, 3, 0.04)
-
-    # Result is dilated for marking the corners, not important
     dst = cv.dilate(dst, None)
-
-    # Threshold for an optimal value, it may vary depending on the image.
     image[dst > 0.01 * dst.max()] = [0, 0, 255]
     name = path + "/" + filename + ".jpg"
     cv.imwrite(name, image)
